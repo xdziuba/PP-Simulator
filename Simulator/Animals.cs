b142bbff5691b146d58fb1369911e8d5e9cc58b1
xdziuba <xdziuba@gmail.com>
2024-12-12 13:20:00 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,9 @@
+﻿namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+
+    public string Info => $"{Description} <{Size}>";
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name = "Default Creature", int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature() { }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+    public string Info => $"{Name} [{Level}]";
+
+}
